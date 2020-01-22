731687669341e1a667c0c53e9ef7c59b8eab23f9
Lata <panwarlsweet@gmail.com>
2020-01-22 11:16:52 +0100

    shifting modules to python folder

---
+++ python/training_utils.py
@@ -0,0 +1,51 @@
+import sys; sys.path.append("/afs/cern.ch/user/l/lata/HHbbggTraining/python")
+#import matplotlib.pyplot as plt
+import os
+import random as rnd
+from sklearn.utils.extmath import cartesian
+import scipy.stats as stats
+import random
+
+# ---------------------------------------------------------------------------------------------------
+class IO:
+    ldata = os.path.expanduser("/afs/cern.ch/user/l/lata/HHbbggTraining/scripts/")
+    #xdata = "/afs/cern.ch/user/i/ivovtin/HHggbb/HHbbggTraining/output_files/"
+    plotFolder = os.path.expanduser("/afs/cern.ch/user/l/lata/HHbbggTraining/scripts/output_files/")
+    signalName = []
+    backgroundName = []
+    dataName = []
+    sigProc = []
+    bkgProc = []
+    dataProc = []
+    nSig=0
+    nBkg=0
+    nData=0
+    signal_df = []
+    background_df = []
+    data_df= []
+    
+    cross_sections = {}
+
+    @staticmethod
+    def add_signal(ntuples,sig, proc):
+        IO.signalName.append(IO.ldata+ntuples+"/"+''.join(sig))
+        IO.sigProc.append(proc)
+        IO.nSig+=1
+	
+    @staticmethod
+    def add_background(ntuples,bkg,proc):
+        IO.backgroundName.append(IO.ldata+ntuples+"/"+''.join(bkg))
+        IO.bkgProc.append(proc)
+        IO.nBkg+=1
+    
+    @staticmethod
+    def add_data(ntuples,data,proc):
+        IO.dataName.append(IO.ldata+ntuples+"/"+''.join(data))
+        IO.dataProc.append(proc)
+        IO.nData+=1
+
+
+
+    
+
+
